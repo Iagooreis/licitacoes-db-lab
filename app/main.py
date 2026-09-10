e871324dc0df0c863a02ea090be587daa73e6455
Iago Reis <iagooreis27@hotmail.com>
2026-09-10 13:20:14 -0300

    feat: adiciona conexao com PostgreSQL

--- app/main.py
+++ app/main.py
@@ -1,13 +1,24 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import Depends, FastAPI
+from sqlalchemy import text
+from sqlalchemy.orm import Session
+
+from app.database import get_session
 
 app = FastAPI(
     title="API de Licitacoes",
     version="0.1.0",
 )
 
+SessionDep = Annotated[Session, Depends(get_session)]
+
 
 @app.get("/health", tags=["infraestrutura"])
-def health_check() -> dict[str, str]:
-    return {"status": "ok"}
+def health_check(session: SessionDep) -> dict[str, str]:
+    session.execute(text("SELECT 1"))
 
-
+    return {
+        "status": "ok",
+        "database": "ok",
+    }
